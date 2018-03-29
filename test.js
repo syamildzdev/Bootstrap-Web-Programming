4d4da4aa68d3f6387236604e64df1d3cf9af77c7
SyamilDz <mdsyamil9@gmail.com>
2018-03-30 00:38:44 +0200

    javascript tuto

---
+++ test.js
@@ -0,0 +1,12 @@
+
+var age = 18;
+var decimal = 3.142;
+var personname = "Hira said, \"Gohool is very cool\"";
+
+document.write(personname);
+
+var bananas = 100;
+var grapes = 20;
+var total = bananas + grapes;
+
+document.write(total);
